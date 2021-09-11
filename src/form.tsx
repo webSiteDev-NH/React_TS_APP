fbe0f393c2860b35d4dd14336ae9dbf072838e51
webSiteDev-NH <nonomurahiroki0807@gmail.com>
2021-09-11 20:18:59 +0900

    フォームの利用

--- src/form.tsx
+++ src/form.tsx
@@ -1,5 +1,29 @@
+import {useState} from 'react';
+
 export const Form = () => {
+
+  const [text, setText] = useState('');
+
+  const submitForm = (event: any) => {
+    event.preventDefault(); //ページ遷移を止める
+    console.log(text);
+  }
+
   return(
-    <div>フォームです</div>
+    <>
+      <h4>新しい言語の追加</h4>
+      <form onSubmit={submitForm}>
+        <div>
+          <input
+            type="text"
+            value={text}
+            onChange={ (e) => { setText( e.target.value ) } }
+          />
+        </div>
+        <div>
+          <button>追加</button>
+        </div>
+      </form>
+    </>
   )
 }
