d51feaac9f2eed3dd670bf311c8026a5386b39e2
webSiteDev-NH <nonomurahiroki0807@gmail.com>
2021-09-15 08:43:56 +0900

    ヘッダーのコンポーネント化

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,8 @@ import { Form } from './form';
 import { getLanguages } from './const/Type';
 import { withLoading } from './hoc/withLoading';
 import { Modal } from './components/modal';
+import { Header } from './Header';
 
-// CSS
-// styled-components
-// コンポーネントとしてHTMLタグとCSSを適用
-/*----------------------------------- */
-const Header = styled.header`
-  display: flex;
-  justyfy-content: space-between;
-  padding: 24px 64px 0;
-  border-bottom: 1px solid #E0E0E0;
-`
-const Headerul = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
-`
-
-// コンポーネント内で使用するプロパティの型宣言しないとエラー
-interface liProps {
-  focused: boolean
-}
-// const Headerli = styled.li<liProps>`
-const Headerli = styled.li<{ focused: boolean }>`
-  list-style: none;
-  padding: 4px 12px;
-  cursor: pointer;
-  border-bottom: ${ ({ focused }) => focused ? '2px solid #F44336' : 'none'};
-`
-/*----------------------------------- */
 
 const App: React.FC<{data: string[]}> = ( {data} ) => {
 
@@ -70,12 +43,7 @@ const App: React.FC<{data: string[]}> = ( {data} ) => {
 
   return (
     <div>
-      <Header>
-        <Headerul>
-          <Headerli focused={tab ==='list'} onClick={() => setTab('list')} >リスト</Headerli>
-          <Headerli focused={tab ==='form'} onClick={() => setTab('form')}>フォーム</Headerli>
-        </Headerul>
-      </Header>
+      <Header tab={tab} setTab={setTab} />
       {
         tab === 'list' ? <List langs={langs} /> : <Form onAddLang={addLang} />
       }
