cafd1c0afa08acf9058d94a898c7250e7e55fab3
webSiteDev-NH <nonomurahiroki0807@gmail.com>
2025-04-29 19:34:39 +0900

    文言変更2

--- src/Header.tsx
+++ src/Header.tsx
@@ -48,7 +48,8 @@ export const Header: React.FC<{tab: string; setTab(param:string): void;}> = ({ta
       <Headerul>
         <Headerli focused={tab ==='list'} onClick={() => setTab('list')} >リストテスト
         </Headerli>
-        <Headerli focused={tab ==='form'} onClick={() => setTab('form')} >フォーム</Headerli>
+        <Headerli focused={tab ==='form'} onClick={() => setTab('form')} >フォームテスト
+        </Headerli>
         <Headerli focused={tab ==='chat'} onClick={() => setTab('chat')} >チャット</Headerli>
       </Headerul>
       <HeaderButton onClick={ (e) => typeof toggleTheme === 'function' ? toggleTheme() : toggleTheme }>
